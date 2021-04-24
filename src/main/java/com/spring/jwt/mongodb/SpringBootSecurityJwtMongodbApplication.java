9bee59c22df26320150a5b14bb6d1ffa1f0ea617
Constantine <linetski@gmail.com>
2021-04-24 19:38:18 +0300

    eureka config added

--- src/main/java/com/spring/jwt/mongodb/SpringBootSecurityJwtMongodbApplication.java
+++ src/main/java/com/spring/jwt/mongodb/SpringBootSecurityJwtMongodbApplication.java
@@ -2,7 +2,9 @@ package com.spring.jwt.mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringBootSecurityJwtMongodbApplication {
 
