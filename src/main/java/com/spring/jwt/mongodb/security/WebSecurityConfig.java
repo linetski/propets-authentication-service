9b8315955dd403d6df68502ecbd9f7f5eddc893b
Constantine <linetski@gmail.com>
2021-04-28 13:56:07 +0300

    1

--- src/main/java/com/spring/jwt/mongodb/security/WebSecurityConfig.java
+++ src/main/java/com/spring/jwt/mongodb/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.spring.jwt.mongodb.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,8 +14,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.spring.jwt.mongodb.security.jwt.AuthEntryPointJwt;
 import com.spring.jwt.mongodb.security.jwt.AuthTokenFilter;
@@ -67,8 +68,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 	
-	/*
-	 * @Override public void addCorsMappings(CorsRegistry registry) {
-	 * registry.addMapping("/**") .allowedOrigins("*") .allowedMethods("*"); }
-	 */
+	 @Bean
+	 public FilterRegistrationBean corsFilter() {
+	     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	     CorsConfiguration config = new CorsConfiguration();
+	     config.setAllowCredentials(true);
+	     config.addAllowedOrigin("*");
+	     config.addAllowedHeader("*");
+	     config.addAllowedMethod("*");
+	     source.registerCorsConfiguration("/**", config);
+	     FilterRegistrationBean bean = new FilterRegistrationBean(new org.springframework.web.filter.CorsFilter(source));
+	     bean.setOrder(0);
+	     return bean;
+	 }
 }
