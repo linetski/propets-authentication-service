f5784087a6bc00720f30eeba2215af3469ca4ca2
Constantine <linetski@gmail.com>
2021-04-28 14:33:58 +0300

    1

--- src/main/java/com/spring/jwt/mongodb/SpringBootSecurityJwtMongodbApplication.java
+++ src/main/java/com/spring/jwt/mongodb/SpringBootSecurityJwtMongodbApplication.java
@@ -2,12 +2,12 @@ package com.spring.jwt.mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -17,23 +17,19 @@ public class SpringBootSecurityJwtMongodbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityJwtMongodbApplication.class, args);
 	}
-	
-	@Bean
-	public CorsFilter corsFilter() {
-	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    final CorsConfiguration config = new CorsConfiguration();
-	    config.setAllowCredentials(true);
-	    config.addAllowedOrigin("*");
-	    config.addAllowedHeader("*");
-	    config.addAllowedMethod("OPTIONS");
-	    config.addAllowedMethod("HEAD");
-	    config.addAllowedMethod("GET");
-	    config.addAllowedMethod("PUT");
-	    config.addAllowedMethod("POST");
-	    config.addAllowedMethod("DELETE");
-	    config.addAllowedMethod("PATCH");
-	    source.registerCorsConfiguration("/**", config);
-	    return new CorsFilter(source);
-	}
+
+	 @Bean
+	 public FilterRegistrationBean corsFilter() {
+	     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	     CorsConfiguration config = new CorsConfiguration();
+	     config.setAllowCredentials(true);
+	     config.addAllowedOrigin("*");
+	     config.addAllowedHeader("*");
+	     config.addAllowedMethod("*");
+	     source.registerCorsConfiguration("/**", config);
+	     FilterRegistrationBean bean = new FilterRegistrationBean(new org.springframework.web.filter.CorsFilter(source));
+	     bean.setOrder(0);
+	     return bean;
+	 }
 
 }
--- src/main/java/com/spring/jwt/mongodb/security/WebSecurityConfig.java
+++ src/main/java/com/spring/jwt/mongodb/security/WebSecurityConfig.java
@@ -1,12 +1,10 @@
 package com.spring.jwt.mongodb.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,12 +12,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
-import com.google.common.collect.ImmutableList;
 import com.spring.jwt.mongodb.security.jwt.AuthEntryPointJwt;
 import com.spring.jwt.mongodb.security.jwt.AuthTokenFilter;
 import com.spring.jwt.mongodb.security.services.UserDetailsServiceImpl;
@@ -66,21 +58,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 	
-	@Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(ImmutableList.of("*"));
-        configuration.setAllowedMethods(ImmutableList.of("HEAD",
-                "GET", "POST", "PUT", "DELETE", "PATCH"));
-        // setAllowCredentials(true) is important, otherwise:
-        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
-        configuration.setAllowCredentials(true);
-        // setAllowedHeaders is important! Without it, OPTIONS preflight request
-        // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/lostAndFound/*", configuration);
-        return source;
-    }
+	
 	
 }
