df0436201ce34addeec9f6bbaa745163829fcddc
Constantine <linetski@gmail.com>
2021-04-28 14:19:10 +0300

    1

--- src/main/java/com/spring/jwt/mongodb/security/WebSecurityConfig.java
+++ src/main/java/com/spring/jwt/mongodb/security/WebSecurityConfig.java
@@ -15,8 +15,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import com.google.common.collect.ImmutableList;
 import com.spring.jwt.mongodb.security.jwt.AuthEntryPointJwt;
 import com.spring.jwt.mongodb.security.jwt.AuthTokenFilter;
 import com.spring.jwt.mongodb.security.services.UserDetailsServiceImpl;
@@ -52,8 +55,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	}
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
+	protected void configure(HttpSecurity http) throws Exception {   
+		http.cors().and()
 		.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 		.authorizeRequests().antMatchers("/api/auth/**").permitAll()
@@ -63,17 +66,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 	
-	 @Bean
-	 public FilterRegistrationBean corsFilter() {
-	     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	     CorsConfiguration config = new CorsConfiguration();
-	     config.setAllowCredentials(true);
-	     config.addAllowedOrigin("*");
-	     config.addAllowedHeader("*");
-	     config.addAllowedMethod("*");
-	     source.registerCorsConfiguration("/**", config);
-	     FilterRegistrationBean bean = new FilterRegistrationBean(new org.springframework.web.filter.CorsFilter(source));
-	     bean.setOrder(0);
-	     return bean;
-	 }
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ImmutableList.of("*"));
+        configuration.setAllowedMethods(ImmutableList.of("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+	
 }
