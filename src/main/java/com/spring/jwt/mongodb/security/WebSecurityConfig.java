baa1eb384aa7c5ed6e105178e5451d8d8a093b25
Constantine <linetski@gmail.com>
2021-04-28 13:36:30 +0300

    1

--- src/main/java/com/spring/jwt/mongodb/security/WebSecurityConfig.java
+++ src/main/java/com/spring/jwt/mongodb/security/WebSecurityConfig.java
@@ -26,7 +26,7 @@ import com.spring.jwt.mongodb.security.services.UserDetailsServiceImpl;
 		// securedEnabled = true,
 		// jsr250Enabled = true,
 		prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer{
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	UserDetailsServiceImpl userDetailsService;
 
@@ -56,11 +56,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/api/auth/**").permitAll()
+		http.cors().and().csrf().disable()
+		.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		.authorizeRequests().antMatchers("/api/auth/**").permitAll()
+		.antMatchers("/api/test/**").permitAll()
 		.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
+	
 	/*
 	 * @Override public void addCorsMappings(CorsRegistry registry) {
 	 * registry.addMapping("/**") .allowedOrigins("*") .allowedMethods("*"); }
