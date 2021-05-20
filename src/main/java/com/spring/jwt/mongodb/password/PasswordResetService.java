1c218eb3e870b4b525e913ffe61406896080e065
Constantine <linetski@gmail.com>
2021-05-20 18:31:55 +0300

    email sending via kafka

--- src/main/java/com/spring/jwt/mongodb/password/PasswordResetService.java
+++ src/main/java/com/spring/jwt/mongodb/password/PasswordResetService.java
@@ -1,29 +1,27 @@
 package com.spring.jwt.mongodb.password;
 
 import java.util.Calendar;
-import java.util.Locale;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import com.spring.jwt.mongodb.models.PasswordResetToken;
 import com.spring.jwt.mongodb.models.User;
 import com.spring.jwt.mongodb.repository.PasswordTokenRepository;
 import com.spring.jwt.mongodb.repository.UserRepository;
 
+import propets.model.Email;
+
 @Component
 public class PasswordResetService {
 	
-	@Autowired
-	PasswordTokenRepository passwordTokenRepository;
+	private static final String EMAIL_TOPIC = "email";
 	
 	@Autowired
-    JavaMailSender emailSender;
+	PasswordTokenRepository passwordTokenRepository;
 
 	@Autowired
 	UserRepository userRepository;
@@ -31,22 +29,16 @@ public class PasswordResetService {
 	@Autowired
 	PasswordEncoder encoder;
 	
+	@Autowired
+    private KafkaTemplate<String, Email> emailKafkaTemplate;
+	
 	public void sendEmail(String token, User user) {
-		emailSender.send(constructResetTokenEmail("heroku", null, token, user));
-	}
-
-	private SimpleMailMessage constructResetTokenEmail(String contextPath, Locale locale, String token, User user) {
-	    String url = "http://localhost:3000" + "/reset_password?token=" + token;
-	    //String message = messages.getMessage("message.resetPassword", null, locale);
-	    return constructEmail("reset password url:",  url, user);
-	}
-
-	private SimpleMailMessage constructEmail(String subject, String body, User user) {
-	    SimpleMailMessage email = new SimpleMailMessage();
-	    email.setSubject(subject);
-	    email.setText(body);
-	    email.setTo(user.getEmail());
-	    return email;
+		String url = "http://localhost:3000" + "/reset_password?token=" + token;
+		Email email = new Email();
+		email.setBody(url);
+		email.setEmailAdress(user.getEmail());
+		email.setSubject("reset password url:");
+		emailKafkaTemplate.send(EMAIL_TOPIC,email);
 	}
 	
 	public void createPasswordResetTokenForUser(User user, String token) {
@@ -71,14 +63,7 @@ public class PasswordResetService {
 	    final Calendar cal = Calendar.getInstance();
 	    return passToken.getExpiryDate().before(cal.getTime());
 	}
-	
-	public JavaMailSender getEmailSender() {
-		return emailSender;
-	}
 
-	public void setEmailSender(JavaMailSender emailSender) {
-		this.emailSender = emailSender;
-	}
 	
 	public void changePassword(String token, String newPassword) {
 		Optional<PasswordResetToken> passwordResetToken = passwordTokenRepository.findByToken(token);
